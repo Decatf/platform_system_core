f8b2c6efef62af92133dcc4e46e04c2cd95ab134
Christopher R. Palmer <crpalmer@gmail.com>
2016-05-16 05:21:28 -0400

    init: Allow devices to opt-out of fsck'ing on power off
    On the zenfone2 the fsck code patch kills alls processes.  This causes
    watchdogd to get killed which then causes the system to emergency reboot
    instead of powering off.
    
    Change-Id: Ibcc74825df6571caa70e2dde7766a72b7749581b

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -110,6 +110,7 @@ done:
     return ret;
 }
 
+#ifndef UMOUNT_AND_FSCK_IS_UNSAFE
 // Turn off backlight while we are performing power down cleanup activities.
 static void turnOffBacklight() {
     static const char off[] = "0";
@@ -137,6 +138,7 @@ static void turnOffBacklight() {
 
     android::base::WriteStringToFile(off, "/sys/devices/virtual/mdnie/mdnie/lcd_power");
 }
+#endif
 
 static int wipe_data_via_recovery(const std::string& reason) {
     const std::vector<std::string> options = {"--wipe_data", std::string() + "--reason=" + reason};
@@ -150,6 +152,7 @@ static int wipe_data_via_recovery(const std::string& reason) {
 }
 
 static void unmount_and_fsck(const struct mntent *entry) {
+#ifndef UMOUNT_AND_FSCK_IS_UNSAFE
     if (strcmp(entry->mnt_type, "f2fs") && strcmp(entry->mnt_type, "ext4"))
         return;
 
@@ -224,6 +227,7 @@ static void unmount_and_fsck(const struct mntent *entry) {
         android_fork_execvp_ext(ARRAY_SIZE(ext4_argv), (char **)ext4_argv,
                                 &st, true, LOG_KLOG, true, NULL, NULL, 0);
     }
+#endif
 }
 
 static int do_class_start(const std::vector<std::string>& args) {
