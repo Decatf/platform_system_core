75221d3ef5a5ca70d4b6d4e29beee06558f6317c
ryang <decatf@gmail.com>
2019-02-22 10:56:07 -0500

    Revert "Turn off lcd with backlight"
    This reverts commit 2aca0f0cb2850e28a2d6615d7bf1170c1d434d63.

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -134,8 +134,6 @@ static void turnOffBacklight() {
                                                            dp->d_name);
         android::base::WriteStringToFile(off, fileName);
     }
-
-    android::base::WriteStringToFile(off, "/sys/devices/virtual/mdnie/mdnie/lcd_power");
 }
 
 static int wipe_data_via_recovery(const std::string& reason) {
